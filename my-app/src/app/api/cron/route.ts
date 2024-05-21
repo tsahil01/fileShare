813d40cba701b8a1266c15171412bb88d79eb87b
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-05-22 00:43:36 +0530

    fixing

--- my-app/src/app/api/cron/route.ts
+++ my-app/src/app/api/cron/route.ts
@@ -1,11 +1,20 @@
-export async function GET() {
-    const result = await fetch(
-      'http://worldtimeapi.org/api/timezone/America/Chicago',
-      {
-        cache: 'no-store',
-      },
-    );
-    const data = await result.json();
-   
-    return Response.json({ datetime: data.datetime });
+import { UTApi } from "uploadthing/server";
+
+export async function DELETE() {
+    try{
+        // const url = data.url.substring(data.url.lastIndexOf("/") + 1);
+        const url = "e9314c87-d073-4f23-a330-5ce3cd9b93e0-65y24m.pdf"
+        const utapi = new UTApi();
+        const res = await utapi.deleteFiles(url);
+        return new Response(JSON.stringify({
+          msg: "Deleted",
+          res: res
+        }))
+      }
+      catch(e){
+        return new Response(JSON.stringify({
+          msg: "Error",
+          error: e
+        }))
+      }
   }
