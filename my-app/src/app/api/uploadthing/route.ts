2efd2dd452edfbde5505a8fa23f312fff177072d
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-05-22 00:38:38 +0530

    uploading thing cron test

--- my-app/src/app/api/uploadthing/route.ts
+++ my-app/src/app/api/uploadthing/route.ts
@@ -12,9 +12,10 @@ export async function DELETE(req: Request){
 
     const data = await req.json();
     try{
-      const url = data.url.substring(data.url.lastIndexOf("/") + 1);
+      // const url = data.url.substring(data.url.lastIndexOf("/") + 1);
+      const url = "e9314c87-d073-4f23-a330-5ce3cd9b93e0-65y24m.pdf"
       const utapi = new UTApi();
-      const res = await utapi.deleteFiles([]);
+      const res = await utapi.deleteFiles(url);
       return new Response(JSON.stringify({
         msg: "Deleted",
         res: res
