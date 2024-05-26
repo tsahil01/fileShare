b48bd3ef3f08cb30a1ebc992e39aee2b091a6124
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-05-26 21:02:32 +0530

    improved styling

--- my-app/src/components/Steps.tsx
+++ my-app/src/components/Steps.tsx
@@ -11,17 +11,18 @@ import React from "react"
 
 export default function Steps() {
     return <>
-    <div className="flex flex-col gap-6 items-center justify-around border-y-2 border-zinc-900 p-3 mt-9">
-        <CardDetails title="Step-1" content="/step1.gif">
+    <div className="flex flex-col gap-0 items-center justify-around border-y-2 border-zinc-900 px-3 mt-9">
+        <CardDetails title="Step-1" content="/step1.gif" classValue="">
             <div className="">
-            <span className="">Go to- </span>
-            <span className="rounded bg-zinc-950 p-2 hover:underline hover:bg-zinc-900">
-                <span className="text-white italic"> ezfiles.vercel.app/ </span>
+            <span className="">Go to: </span>
+            <span className="rounded bg-zinc-950 hover:underline hover:bg-zinc-900">
+                <span className="text-white italic">ezfiles.vercel.app/ </span>
                 <span className="text-white italic font-bold">anyRandomString</span>
             </span>
             </div>
         </CardDetails>
-        <CardDetails title="Step-2" content="/step1.gif">
+
+        <CardDetails title="Step-2" content="/step1.gif" classValue="">
             <div className="">
             <span className="">Upload your file </span>
             </div>
@@ -32,7 +33,7 @@ export default function Steps() {
 
 function CardDetails({title, content, footer, classValue, children }: {title?: string, content?: string, footer?: string, classValue?: string, children: React.ReactNode}){
     return <>
-    <Card className={`w-full rounded-none border-zinc-900 border-x-2 ${classValue}`}>
+    <Card className={`w-full rounded-none border-zinc-900 border-0 border-x-2 border-b-2 pt-3 ${classValue}`}>
         <CardHeader className="p-3">
             <CardTitle className="text-2xl text-transparent bg-clip-text bg-gradient-to-b from-zinc-100 to-zinc-700">{title}</CardTitle>
             <CardDescription className="text-sm text-zinc-500">{children}</CardDescription>
