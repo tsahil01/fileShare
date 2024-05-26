2c42d6cb90851ef03341e373d21a9dede4c7db0b
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-05-26 15:45:21 +0530

    fix ui styling

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import VideoPlayer from "@/components/VideoPlayer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full bg-black text-white min-h-screen px-2">
+    <main className="flex flex-col w-full bg-zinc-950 text-white min-h-screen px-2">
         <div className="md:max-w-screen-2xl md:pt-36 pt-20 sm:pb-20 md:pb-10 mx-auto sm:mx-12 text-white border-x-2 border-zinc-900 min-h-screen">
         {/* <hr className="h-px border-0 bg-gray-800 mb-1"/> */}
         <Header />
--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -3,11 +3,11 @@ export default function Header() {
         <>
         <div className="flex flex-col items-center justify-center border-y-2 border-zinc-900 py-2">
             <h1 className="font-bold md:text-6xl text-3xl text-center px-3">
-                <span className="text-transparent bg-clip-text bg-gradient-to-b from-zinc-400 to-zinc-950">Effortlessly </span>  
-                <span className="text-transparent bg-clip-text bg-gradient-to-t from-white to-gray-400">Share Your Files </span> 
-                <span className="text-transparent bg-clip-text bg-gradient-to-b from-zinc-400 to-zinc-950">Anywhere!</span> 
+                <span className="text-transparent bg-clip-text bg-gradient-to-l from-zinc-400 to-zinc-800">Effortlessly </span>  
+                <span className="text-transparent bg-clip-text bg-gradient-to-b from-zinc-100 to-zinc-200">Share Your Files </span> 
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-400 to-zinc-800">Anywhere!</span> 
             </h1>    
-            <div className="text-xs md:text-xl text-zinc-400 text-center max-w-sm md:max-w-2xl mt-4 md:px-4 px-7">Simplify your file sharing experience with our intuitive platform. Seamlessly upload, share, and access your files anytime, anywhere. Start sharing smarter today!</div>
+            <div className="text-xs md:text-xl text-zinc-400 text-center max-w-sm md:max-w-2xl mt-4 md:px-4 px-7 text-transparent bg-clip-text bg-gradient-to-b from-zinc-200 to-zinc-800">Simplify your file sharing experience with our intuitive platform. Seamlessly upload, share, and access your files anytime, anywhere. Start sharing smarter today!</div>
         </div>
         </>
        );
